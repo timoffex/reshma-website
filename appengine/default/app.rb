beccf409fc161b547fe13e99ecfef8c4f5eb88e4
Timofey Peshin <timoffex@gmail.com>
2021-10-28 22:17:27 -0700

    Update Ruby style

--- appengine/default/app.rb
+++ appengine/default/app.rb
@@ -1,12 +1,11 @@
-# [START app]
-require "sinatra"
-require "google/cloud/storage"
+require 'sinatra'
+require 'google/cloud/storage'
 
 
 class SchemaGetter
   def initialize
     storage = Google::Cloud::Storage.new
-    @bucket = storage.bucket "reshma-website.appspot.com"
+    @bucket = storage.bucket('reshma-website.appspot.com')
     @last_read_time = Time.now
     @last_schema = nil
   end
@@ -14,8 +13,8 @@ class SchemaGetter
   def get_schema
     # Refresh schema at most once every minute
     if @last_schema.nil? || Time.now > @last_read_time + 60
-      puts "Updating schema"
-      schema_file = @bucket.file("schema.json").download
+      puts 'Updating schema'
+      schema_file = @bucket.file('schema.json').download
       schema_file.rewind
       @last_schema = schema_file.read
     end
@@ -28,18 +27,17 @@ schema_getter = SchemaGetter.new
 
 
 
-get "/" do
+get '/' do
   send_file File.expand_path('index.html', settings.public_folder)
 end
 
-get "/schema" do
+get '/schema' do
   content_type :json
   schema_getter.get_schema
 end
 
 # App Engine uses the health check to ensure that the instance is healthy
 # and ready to serve traffic.
-get "/_ah/health" do
-  "ok"
+get '/_ah/health' do
+  'ok'
 end
-# [END app]
--- appengine/editor/app.rb
+++ appengine/editor/app.rb
@@ -1,12 +1,11 @@
-# [START app]
-require "sinatra"
-require "google/cloud/storage"
+require 'sinatra'
+require 'google/cloud/storage'
 
 
 class SchemaService
   def initialize
     storage = Google::Cloud::Storage.new
-    @dev_bucket = storage.bucket "reshma-website-dev"
+    @dev_bucket = storage.bucket('reshma-website-dev')
     @last_read_time = Time.now
     @last_schema = nil
   end
@@ -14,8 +13,8 @@ class SchemaService
   def get_schema
     # Refresh schema at most once every minute
     if @last_schema.nil? || Time.now > @last_read_time + 60
-      puts "Updating schema"
-      schema_file = @dev_bucket.file("schema.json").download
+      puts 'Updating schema'
+      schema_file = @dev_bucket.file('schema.json').download
       schema_file.rewind
       @last_schema = schema_file.read
     end
@@ -26,8 +25,8 @@ class SchemaService
   def put_schema(schema_json)
     @last_schema = schema_json
 
-    data = StringIO.new schema_json
-    @dev_bucket.create_file data, "schema.json"
+    data = StringIO.new(schema_json)
+    @dev_bucket.create_file(data, 'schema.json')
   end
 end
 
@@ -36,25 +35,24 @@ schema_service = SchemaService.new
 
 # TODO: Require credentials
 
-get "/" do
+get '/' do
   send_file File.expand_path('index.html', settings.public_folder)
 end
 
-get "/schema" do
+get '/schema' do
   content_type :json
   schema_service.get_schema
 end
 
-put "/schema" do
+put '/schema' do
   request.body.rewind
   schema_json = request.body.read
-  schema_service.put_schema schema_json
+  schema_service.put_schema(schema_json)
   status 200
 end
 
 # App Engine uses the health check to ensure that the instance is healthy
 # and ready to serve traffic.
-get "/_ah/health" do
-  "ok"
+get '/_ah/health' do
+  'ok'
 end
-# [END app]
